1f2b87e662abbc00865536a66e306585dd60fc0f
Neit-Shu <Neit.Shu@gmail.com>
2025-12-02 10:08:46 +0300

    add scripts to menu buttons

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;                                             
+
+public class MainMenuManager : MonoBehaviour                   
+{
+    public void StartGame()                                    
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1); 
+    }
+
+    public void QuitGame()                                     
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();          
+#endif
+        Application.Quit();                                    
+    }
+}
